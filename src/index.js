d60b68520da3ad8238f58423af9d023a5ca1d3bb
Ivan <i.runets95@gmail.com>
2018-02-20 02:08:43 +0300

    cant sort

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.array = [];
+    this.arrLength = 0;
   }
 
   add(element) {
     // your implementation
+    this.array.push(element);
+    this.arrLength++;
   }
 
   at(index) {
     // your implementation
+    return this.array[index];
   }
 
   get length() {
     // your implementation
+    return this.arrLength;
   }
 
   toArray() {
     // your implementation
+    return this.array;
   }
 
   sort(indices) {
     // your implementation
+    return this.array.sort(function(a,b) {
+      return a-b;
+    })
   }
 
   setComparator(compareFunction) {
